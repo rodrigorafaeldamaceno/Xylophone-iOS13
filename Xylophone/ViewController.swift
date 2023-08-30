df5cd48178eb3a3d2b0db44d26a87757a2b6ddb6
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2023-08-30 17:34:35 -0300

    fineshed app

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,38 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    var player: AVAudioPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) {
+        let soundName = sender.currentTitle
+        if(soundName==nil) {return}
+                
+        playSound(soundName: sender.titleLabel!.text!)
+        sender.layer.opacity = 0.8;
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+            sender.layer.opacity = 1.0
+        }
+    }
     
+    func playSound(soundName: String) {
+        guard let path = Bundle.main.path(forResource: soundName, ofType: "wav") else { return }
+        let url = URL(fileURLWithPath: path)
+
+        do {
+            player = try AVAudioPlayer(contentsOf: url)
+            player?.play()   
+        } catch let error {
+            print(error.localizedDescription)
+        }  
+    }
 
 }
 
